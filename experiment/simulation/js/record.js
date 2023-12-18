f5f4e73cbbc6f51dedb4a94d1d817548bc140276
unknown <vlabiitk@gmail.com>
2023-12-18 11:16:43 +0530

    update

--- experiment/simulation/js/record.js
+++ experiment/simulation/js/record.js
@@ -7,50 +7,41 @@ const showVideoButton = document.getElementById("button2");
 
 function datasheet(){
  
+  document.getElementById("n").innerHTML = "0.5";
  
-  document.getElementById("n").innerHTML = "1124";
- 
-  document.getElementById("r1").innerHTML = "21.5";
-  document.getElementById("r2").innerHTML = "4";
-  document.getElementById("r").innerHTML = "17.5";
+  document.getElementById("r1").innerHTML = "0.038";
+  document.getElementById("r2").innerHTML = "0.150";
+  document.getElementById("r").innerHTML = "0.112";
   document.getElementById("t").innerHTML = "30";
  // document.getElementById("f1").innerHTML = "30";
   //document.getElementById("f2").innerHTML = "30";
   //document.getElementById("w").innerHTML = "30";
   //document.getElementById("fth").innerHTML = "30";
 
-  document.getElementById("nb").innerHTML = "1522";
+  document.getElementById("nb").innerHTML = "0.7";
  
-  document.getElementById("r1b").innerHTML = "31";
-  document.getElementById("r2b").innerHTML = "3";
-  document.getElementById("rb").innerHTML = "28";
+  document.getElementById("r1b").innerHTML = "0.021";
+  document.getElementById("r2b").innerHTML = "0.153";
+  document.getElementById("rb").innerHTML = "0.132";
   document.getElementById("tb").innerHTML = "30";
   //document.getElementById("f1b").innerHTML = "30";
   //document.getElementById("f2b").innerHTML = "30";
   //document.getElementById("wb").innerHTML = "30";
   //document.getElementById("fthb").innerHTML = "30";
 
-  document.getElementById("nc").innerHTML = "1847";
+
+  document.getElementById("nc").innerHTML = "1";
  
-  document.getElementById("r1c").innerHTML = "40";
-  document.getElementById("r2c").innerHTML = "3";
-  document.getElementById("rc").innerHTML = "37";
+  document.getElementById("r1c").innerHTML = "0.045";
+  document.getElementById("r2c").innerHTML = "0.202";
+  document.getElementById("rc").innerHTML = "0.157";
   document.getElementById("tc").innerHTML = "30";
   //document.getElementById("f1c").innerHTML = "30";
   //document.getElementById("f2c").innerHTML = "30";
   //document.getElementById("wc").innerHTML = "30";
   //document.getElementById("fthc").innerHTML = "30";
 
-  document.getElementById("nd").innerHTML = "2775";
- 
-  document.getElementById("r1d").innerHTML = "48";
-  document.getElementById("r2d").innerHTML = "3";
-  document.getElementById("rd").innerHTML = "45";
-  document.getElementById("td").innerHTML = "30";
-  //document.getElementById("f1d").innerHTML = "30";
-  //document.getElementById("f2d").innerHTML = "30";
-  //document.getElementById("wd").innerHTML = "30";
-  //document.getElementById("fthd").innerHTML = "30";
+
   
 
  
